951630ce7fdd2a384fbee9ac665a7638d63a6f05
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2022-11-05 10:40:26 +0530

    go.mod package.json go.sum package.lock.json

--- db/main.go
+++ db/main.go
@@ -1,68 +1,70 @@
 package main
 
-import(
-   "fmt"
-   "os"
-   "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sync"
+	"github.com/jcelliott/lumber" // Logger package
 )
 
-type Address struct{
-	City string
-	State string
+const Version = "1.0.0"
+
+type Address struct {
+	City    string
+	State   string
 	Country string
 	Pincode json.Number
 }
 
-type User struct{
-	Name string
-	Age json.Number
+type User struct {
+	Name    string
+	Age     json.Number
 	Contact string
 	Company string
 	Address Address
 }
 
-
 func main() {
 
-     dir := "./"
+	dir := "./"
 
-	 db, err := New(dir,nil)
+	db, err := New(dir, nil)
 
-	 if err != nil{
+	if err != nil {
 		fmt.Println("Error", err)
-	 }
-  
-	 employees := []User{
-		{"Anurag","22","32334","Palantir",Address{"Bangalore","Karnataka","India","5600001"}},
-		{"Anitej","32","2234","Razorpay",Address{"Kolkata","West Bengal","India","700025"}},
-		{"Murali","42","2234","Amazon",Address{"Hardoi","Uttar Pradesh","India","241001"}},
-		{"JSON","52","6234","Statham",Address{"Varkala","Kerala","India","630001"}},
-	 }
+	}
 
-    for _, value := range employees{
+	employees := []User{
+		{"Anurag", "22", "32334", "Palantir", Address{"Bangalore", "Karnataka", "India", "5600001"}},
+		{"Anitej", "32", "2234", "Razorpay", Address{"Kolkata", "West Bengal", "India", "700025"}},
+		{"Murali", "42", "2234", "Amazon", Address{"Hardoi", "Uttar Pradesh", "India", "241001"}},
+		{"JSON", "52", "6234", "Statham", Address{"Varkala", "Kerala", "India", "630001"}},
+	}
+
+	for _, value := range employees {
 		db.Write("users", value.Name, User{
-			Name :  value.Name,
-			Age : value.Age,
-			Contact : value.Contact,
+			Name:    value.Name,
+			Age:     value.Age,
+			Contact: value.Contact,
 			Company: value.Company,
 			Address: value.Address,
-		
 		})
 	}
 
 	records, err := db.RealAll("users")
 
-	if err != nil{
-		fmt.Println("Error",err)
-	}else{
+	if err != nil {
+		fmt.Println("Error", err)
+	} else {
 		fmt.Println(records)
 	}
-    
+
 	allUsers := []User{}
 
-	for _,record := range records{
-        employeeFound := User{}
-		if err:= json.Unmarshal([]byte(record),&employeeFound); err!=nil{
+	for _, record := range records {
+		employeeFound := User{}
+		if err := json.Unmarshal([]byte(record), &employeeFound); err != nil {
 			fmt.Println("Error", err)
 		}
 		allUsers = append(allUsers, employeeFound)
@@ -70,12 +72,12 @@ func main() {
 
 	fmt.Println((allUsers))
 
-	if err := db.Delete("user","Anitej");err!=nil{
-      fmt.Println("Error", err)
-	}
+	// if err := db.Delete("user", "Anitej"); err != nil {
+	// 	fmt.Println("Error", err)
+	// }
 
-	if err:= db.Delete("user",""); err!=nil{
-		fmt.Println("Error", err)
-	}
-	
+	// if err := db.Delete("user", ""); err != nil {
+	// 	fmt.Println("Error", err)
+	// }
+
 }
